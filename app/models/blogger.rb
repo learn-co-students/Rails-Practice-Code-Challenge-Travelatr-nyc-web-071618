ccaac535082bccdad6bef21077df01d56f1b687e
github username <github email address>
2018-08-17 16:44:10 -0400

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,28 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
+
+  validates :name, uniqueness: true
+  validates :age, numericality: {minimum: 0}
+  validates :bio, length: {minimum: 30}
+
+  def featured_post
+    like_count = -1
+    self.posts.each do |post|
+      if post.likes > like_count
+        like_count = post.likes
+        @hot_post = post
+      end
+    end
+    @hot_post
+  end
+
+  def total_likes
+    @likes = 0
+    self.posts.each do |post|
+      @likes += post.likes
+    end
+    @likes
+  end
 
 end
